498ed97b9e4e53ea88b23ee8df9fb456a48553e8
tushargithub44 <tushar.bapecha@somaiya.edu>
2020-03-31 00:29:57 +0530

    Templates added

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,18 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, Tushar!"
+    user = {'username' : 'Tushar'}
+    posts = [
+        {
+            'Author': {'username': 'Talha'},
+            'Caption': 'Soft Computing has great Advantages'
+        },
+        {
+            'Author': {'username': 'Rugved'},
+            'Caption': 'Concurrency Control is nice topic!'
+        }
+    ]
+    return render_template('index.html',title = "Home", user = user, posts = posts)
