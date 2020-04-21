b851d14033ef006e0910a29a70aa5565d91ea941
tushargithub44 <tushar.bapecha@somaiya.edu>
2020-04-21 08:37:08 +0530

    Date time Format changed

--- app/routes.py
+++ app/routes.py
@@ -108,14 +108,14 @@ def edit_profile():
 def follow(username):
     user = User.query.filter_by(username=username).first()
     if user is None:
-        flash('User {} not found!',format(username))
+        flash('User {} not found!'.format(username))
         return redirect(url_for('index'))
     if user == current_user:
         flash('You cannot follow yourself!')
         return redirect(url_for('user',username=username))
     current_user.follow(user)
     db.session.commit()
-    flash('You are Following {}!',format(username))
+    flash('You are Following {}!'.format(username))
     return redirect(url_for('user',username=username))
 
 @app.route('/unfollow/<username>')
--- config.py
+++ config.py
@@ -12,7 +12,7 @@ class Config(object):
     MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-    ADMINS = ['tata8149417794@gmail.com']
+    ADMINS = ['abc4@gmail.com']
     POSTS_PER_PAGE = 3
 
     
