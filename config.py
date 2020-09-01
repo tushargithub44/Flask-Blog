2449cb1d258ebfeb388f44e835335dd7abd1f444
tushargithub44 <tushar.bapecha@somaiya.edu>
2020-09-01 14:46:14 +0530

    bug fixed

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 load_dotenv(os.path.join(basedir, '.env'))
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hello111'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
         'sqlite:///' + os.path.join(basedir,'app.db')
 
@@ -17,7 +17,7 @@ class Config(object):
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     LANGUAGES = ['en']
     ELASTICSEARCH_URL = os.environ.get('ELASTICSEARCH_URL')
-    ADMINS = ['abc4@gmail.com']
+    ADMINS = os.environ.get('ADMINS')
     LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
     POSTS_PER_PAGE = 25
 
